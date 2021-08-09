27540bd5e99c91646afb7f24742f64a1386b91ca
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-09 13:23:40 -0700

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array:', languages);
+
+var firstElement = languages[0];
+console.log('first element of languages array:', firstElement);
+
+var thirdElement = languages[2];
+console.log('third element of languages array:', thirdElement);
+
+var length = languages.length;
+console.log('length of languages array:', length);
+
+var lastElement = languages[languages.length - 1];
+console.log('last element of languages array:', lastElement);
