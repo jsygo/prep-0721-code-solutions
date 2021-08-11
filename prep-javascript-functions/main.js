34fb7e2348e816d9ad190642a1ab390ad0426758
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-10 20:43:38 -0700

    Completed convertHoursToMinutes.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var hoursToMinutes = convertHoursToMinutes(2);
+console.log('output of convertHoursToMinutes(2)', hoursToMinutes);
